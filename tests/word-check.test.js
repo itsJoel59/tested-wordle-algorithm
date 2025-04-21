cc367787d62034f89995537031c05ed341273ff0
itsJoel59 <joel.j.jansson@gmail.com>
2025-04-21 19:34:47 +0200

    Updated test file

--- tests/word-check.test.js
+++ tests/word-check.test.js
@@ -1,5 +1,5 @@
 const check = require('../src/word-check.js');
-
+//Detta test ser till att en gissning med bokstavsdublett räknas ut korrekt
 test('compares the guessedWord HALLÅ with the correctWord CYKLA', () => {
     expect(check('HALLÅ', 'CYKLA')).toEqual(
         [
@@ -26,6 +26,7 @@ test('compares the guessedWord HALLÅ with the correctWord CYKLA', () => {
         ]
     );
 });
+//Detta test bekräftar att korrekt gissning får passande resultat
 test('compares the guessedWord CYKLA with the correctWord CYKLA', () => {
     expect(check('CYKLA', 'CYKLA')).toEqual(
         [
@@ -48,6 +49,33 @@ test('compares the guessedWord CYKLA with the correctWord CYKLA', () => {
             {
                 letter: 'A',
                 result: 'correct'
+            }
+        ]
+    );
+});
+//Detta test testar att inkorrekt gissning får passande resultat
+test('compares the guessedWord FÖRST with the correctWord CYKLA', () => {
+    expect(check('FÖRST', 'CYKLA')).toEqual(
+        [
+            {
+                letter: 'F',
+                result: 'incorrect'
+            },
+            {
+                letter: 'Ö',
+                result: 'incorrect'
+            },
+            {
+                letter: 'R',
+                result: 'incorrect'
+            },
+            {
+                letter: 'S',
+                result: 'incorrect'
+            },
+            {
+                letter: 'T',
+                result: 'incorrect'
             }
         ]
     );
